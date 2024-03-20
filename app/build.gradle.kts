039ad5030b1c278d04db1c1308667e1a74c4ab2d
semihgul5 <semih.gul099@gmail.com>
2024-03-20 22:18:33 +0300

    tflite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
     }
     buildFeatures {
         viewBinding=true
+        mlModelBinding = true
     }
 }
 
@@ -40,9 +41,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
-    implementation("com.android.volley:volley:1.2.1")
+    implementation("org.tensorflow:tensorflow-lite:2.6.0")
+
 }
--- app/src/main/java/com/abrebo/mlmodelwithandroidapp/MainActivity.java
+++ app/src/main/java/com/abrebo/mlmodelwithandroidapp/MainActivity.java
@@ -1,68 +1,98 @@
 package com.abrebo.mlmodelwithandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
-
 import com.abrebo.mlmodelwithandroidapp.databinding.ActivityMainBinding;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    String url="https://student-android-app-pred-810c0241ee1c.herokuapp.com/tah";
+    private Interpreter tflite;
+    String modelFile = "converted_model.tflite";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.button.setOnClickListener(view -> {
-            StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject jsonObject=new JSONObject(response);
-                        String prediction = jsonObject.getString("prediction");
-                        binding.textViewTahmin.setText(prediction);
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
+        // TensorFlow Lite modelini yükleme
+        try {
+            tflite = new Interpreter(loadModelFile(this, modelFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }){
-                @Override
-                protected Map<String ,String> getParams(){
-                    Map<String,String> params=new HashMap<String,String>();
-                    params.put("cgpa",binding.editTextCgpa.getText().toString());
-                    params.put("iq",binding.editTextIq.getText().toString());
-                    params.put("profile_score",binding.editTextProfileScore.getText().toString());
-                    return params;
-                }
-            };
-            RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
-            queue.add(stringRequest);
+        // Tahmin butonuna tıklama olayı
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Kullanıcı girdilerini al
+                float cgpa = Float.parseFloat(binding.editTextCgpa.getText().toString());
+                float iq = Float.parseFloat(binding.editTextIq.getText().toString());
+                float profile_score = Float.parseFloat(binding.editTextProfileScore.getText().toString());
+
+                // Giriş verisini modele uygun formata getir
+                float[][] input = {{cgpa, iq, profile_score}}; // Örnek giriş verisi, modelinize göre değişebilir
+
+                // Modeli çalıştır ve sonucu al
+                float[][] output = runModel(input);
+
+                // Sonucu kullanma
+                float result = output[0][0]; // Örnek çıktıyı alın, modelinize göre çıktıyı işleyebilirsiniz
+
+                // Sonucu gösterme
+                // Sonucu yuvarlayarak gösterme
+                float roundedResult = Math.round(result);
+                String resultMessage = "Tahmin sonucu: " + roundedResult;
+                Toast.makeText(MainActivity.this, resultMessage, Toast.LENGTH_SHORT).show();
+                binding.textViewTahmin.setText(resultMessage);
+            }
         });
+    }
 
+    // Modeli yükleme
+    private MappedByteBuffer loadModelFile(Activity activity, String modelFile) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelFile);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
 
+    // Modeli çalıştırma
+    private float[][] runModel(float[][] input) {
+        // Giriş verisini modelinize uygun hale getirme
+        TensorBuffer inputBuffer = TensorBuffer.createFixedSize(new int[]{input.length, input[0].length}, DataType.FLOAT32);
 
+        // Dönüşümü gerçekleştirme
+        float[] flatInput = new float[input.length * input[0].length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                flatInput[i * input[i].length + j] = input[i][j];
+            }
+        }
 
+        // Tek boyutlu diziyi TensorBuffer'a yükleme
+        inputBuffer.loadArray(flatInput);
 
+        // Modeli çalıştırma
+        TensorBuffer outputBuffer = TensorBuffer.createFixedSize(new int[]{input.length, /* çıktı boyutu */}, DataType.FLOAT32);
+        tflite.run(inputBuffer.getBuffer(), outputBuffer.getBuffer());
+
+        // Çıktıyı alarak işleme
+        float[][] output = new float[][]{outputBuffer.getFloatArray()};
+        return output;
     }
 }
